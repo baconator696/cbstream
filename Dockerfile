622edbfdbbc72a20de80fd5a8855e495f30c3433
baconator696 <169552784+baconator696@users.noreply.github.com>
2025-05-20 01:05:03 +0000

    added support for external mkvtoolnix muxing

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM --platform=linux/amd64 rust AS build
+FROM --platform=$BUILDPLATFORM rust AS build
 ARG TARGETPLATFORM
+ARG BUILDPLATFORM
 WORKDIR /build/
 COPY . /build/
-RUN ./docker-build.sh $TARGETPLATFORM
+RUN bash docker-build.sh $TARGETPLATFORM $BUILDPLATFORM
 FROM scratch
-LABEL org.opencontainers.image.source https://github.com/baconator696/cbstream
+LABEL org.opencontainers.image.source=https://github.com/baconator696/cbstream
 WORKDIR /
 COPY --from=build /target/root /
 COPY --from=build /etc/ssl /etc/ssl
 COPY --from=build /target/cbstream /bin/cbstream
+COPY --from=build /target/mkvmerge /bin/mkvmerge
 ENTRYPOINT ["cbstream"]
--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,8 @@
+echo $2
+if [ "$2" != "linux/amd64" ]; then
+    echo "Unsupported platform: $2"
+    exit 1
+fi
 # arm64 build
 if [ "$1" = "linux/arm64" ]; then
     # installs dependencies
@@ -17,15 +22,40 @@ if [ "$1" = "linux/arm64" ]; then
         cp --parents /lib/aarch64-linux-gnu/libgcc_s.so.1 /target/root/ &&
         cp --parents /lib/aarch64-linux-gnu/libc.so.6 /target/root/ &&
         cp --parents /lib/ld-linux-aarch64.so.1 /target/root/
+    apt install wget -y &&
+        wget -O /etc/apt/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg &&
+        echo "deb [signed-by=/etc/apt/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/debian/ bookworm main" >/etc/apt/sources.list.d/mkvtoolnix.download.list &&
+        apt update && apt install mkvtoolnix:arm64 -y &&
+        mv /bin/mkvmerge /target/mkvmerge &&
+        cp --parents /lib/aarch64-linux-gnu/libboost_filesystem.so.1.74.0 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libFLAC.so.12 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libz.so.1 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libfmt.so.9 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libQt6Core.so.6 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libgmp.so.10 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libstdc++.so.6 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libdvdread.so.8 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libvorbis.so.0 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libogg.so.0 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libm.so.6 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libgcc_s.so.1 /target/root/ &&
+        cp --parents /lib/aarch64-linux-gnu/libdl.so.2 /target/root/
 # amd64 build
 elif [ "$1" = "linux/amd64" ]; then
-    # isntall dependencies and compile
+    # install dependencies and compile
     apt update && apt install gcc libssl-dev pkg-config -y &&
         cargo build -r &&
         mkdir /target &&
         mv /build/target/release/cbstream-rust /target/cbstream &&
         mkdir /target/root &&
         ldd /target/cbstream | grep -o '/[^ ]*' | xargs -I {} cp --parents {} /target/root/
+    # install mkvtoolnix
+    apt install wget -y &&
+        wget -O /etc/apt/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg &&
+        echo "deb [signed-by=/etc/apt/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/debian/ bookworm main" >/etc/apt/sources.list.d/mkvtoolnix.download.list &&
+        apt update && apt install mkvtoolnix -y &&
+        mv /bin/mkvmerge /target/mkvmerge &&
+        ldd /target/mkvmerge | grep -o '/[^ ]*' | xargs -I {} cp --parents {} /target/root/
 else
     echo "Unsupported platform: $1"
     exit 1
