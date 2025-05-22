2ea6ef95088e1ac2094044822c11ec1ebd7a83a0
baconator696 <169552784+baconator696@users.noreply.github.com>
2025-05-22 20:47:25 +0000

    scvr mistake in diff

--- src/platforms/scvr.rs
+++ src/platforms/scvr.rs
@@ -3,7 +3,7 @@ use crate::stream::{Playlist, Stream};
 use std::*;
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 pub fn get_playlist(username: &str) -> Result<Option<String>> {
-    sc::sc_get_playlist(username, false)
+    sc::sc_get_playlist(username, true)
 }
 pub fn parse_playlist(playlist: &mut Playlist) -> Result<Vec<Stream>> {
     sc::sc_parse_playlist(playlist, true)
