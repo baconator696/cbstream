95da2b6120fa051b6d89bf60ec26b887596cb883
baconator696 <169552784+baconator696@users.noreply.github.com>
2025-05-28 18:08:43 +0000

    added headers to http requests, added a post request function, and a global variable for useragent

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,38 @@
-use crate::e;
-use std::{io::Read, *};
+use crate::{e, o, s};
+use std::sync::{Arc, OnceLock, RwLock};
+use std::{collections::HashMap, io::Read, *};
+
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
+
 pub const SLASH: &str = if cfg!(target_os = "windows") { "\\" } else { "/" };
-pub fn get_retry(url: &str, retry: i32) -> Result<String> {
-    let f = |url| {
-        let resp = reqwest::blocking::get(url).map_err(e!())?;
+
+static USERAGENT: OnceLock<Arc<RwLock<String>>> = OnceLock::new();
+pub fn set_useragent(useragent: String) -> Result<()> {
+    let u = USERAGENT.get_or_init(|| Arc::new(RwLock::new(String::new())));
+    *u.write().map_err(s!())? = useragent;
+    Ok(())
+}
+pub fn get_useragent() -> Result<String> {
+    let u = USERAGENT.get().ok_or_else(o!())?;
+    Ok((*u.read().map_err(s!())?).clone())
+}
+
+pub fn get_retry(url: &str, retry: i32, headers: Option<&HashMap<String, String>>) -> Result<String> {
+    let f = || {
+        let client = reqwest::blocking::Client::new();
+        let build = if let Some(headers) = headers {
+            let mut h = reqwest::header::HeaderMap::new();
+            use reqwest::header::HeaderName;
+            use reqwest::header::HeaderValue;
+            use std::str::FromStr;
+            for (k, v) in headers {
+                h.insert(HeaderName::from_str(&k).map_err(e!())?, HeaderValue::from_str(&v).map_err(e!())?);
+            }
+            client.get(url).headers(h)
+        } else {
+            client.get(url)
+        };
+        let resp = build.send().map_err(e!())?;
         let resp_code = resp.status();
         let resp_text = if let Some(v) = resp.headers().get("content-encoding") {
             if v.to_str().map_err(e!())? == "gzip" {
@@ -26,7 +54,7 @@ pub fn get_retry(url: &str, retry: i32) -> Result<String> {
     };
     let mut r = Err("".into());
     for _ in 0..retry {
-        r = f(url);
+        r = f();
         if r.is_ok() {
             break;
         }
@@ -34,9 +62,22 @@ pub fn get_retry(url: &str, retry: i32) -> Result<String> {
     }
     r
 }
-pub fn get_retry_vec(url: &str, retry: i32) -> Result<Vec<u8>> {
+pub fn get_retry_vec(url: &str, retry: i32, headers: Option<&HashMap<String, String>>) -> Result<Vec<u8>> {
     let f = |url| {
-        let resp = reqwest::blocking::get(url).map_err(e!())?;
+        let client = reqwest::blocking::Client::new();
+        let build = if let Some(headers) = headers {
+            let mut h = reqwest::header::HeaderMap::new();
+            use reqwest::header::HeaderName;
+            use reqwest::header::HeaderValue;
+            use std::str::FromStr;
+            for (k, v) in headers {
+                h.insert(HeaderName::from_str(&k).map_err(e!())?, HeaderValue::from_str(&v).map_err(e!())?);
+            }
+            client.get(url).headers(h)
+        } else {
+            client.get(url)
+        };
+        let resp = build.send().map_err(e!())?;
         let resp_code = resp.status();
         if resp_code != 200 {
             return Err(format!("{}|{}", resp.text().map_err(e!())?.trim(), resp_code))?;
@@ -52,6 +93,62 @@ pub fn get_retry_vec(url: &str, retry: i32) -> Result<Vec<u8>> {
         thread::sleep(time::Duration::from_millis(250));
     }
     r
+}
+pub fn post_retry(url: &str, retry: i32, headers: Option<&HashMap<String, String>>, payload: &str, content_type: &str) -> Result<String> {
+    let f = || {
+        let client = reqwest::blocking::Client::new();
+        let build = if let Some(headers) = headers {
+            let mut h = reqwest::header::HeaderMap::new();
+            use reqwest::header::HeaderName;
+            use reqwest::header::HeaderValue;
+            use std::str::FromStr;
+            for (k, v) in headers {
+                h.insert(HeaderName::from_str(&k).map_err(e!())?, HeaderValue::from_str(&v).map_err(e!())?);
+            }
+            client.get(url).headers(h)
+        } else {
+            client.get(url)
+        };
+        let resp = build
+            .body(payload.to_string())
+            .header("content-type", content_type)
+            .send()
+            .map_err(e!())?;
+        let resp_code = resp.status();
+        let resp_text = if let Some(v) = resp.headers().get("content-encoding") {
+            if v.to_str().map_err(e!())? == "gzip" {
+                let resp_data = resp.bytes().map_err(e!())?.to_vec();
+                let mut resp_text = String::new();
+                let mut decoder = flate2::read::GzDecoder::new(resp_data.as_slice());
+                decoder.read_to_string(&mut resp_text).map_err(e!())?;
+                resp_text
+            } else {
+                resp.text().map_err(e!())?
+            }
+        } else {
+            resp.text().map_err(e!())?
+        };
+        if resp_code != 200 {
+            return Err(format!("{}", resp_code))?;
+        }
+        Ok(resp_text)
+    };
+    let mut r = Err("".into());
+    for _ in 0..retry {
+        r = f();
+        if r.is_ok() {
+            break;
+        }
+        thread::sleep(time::Duration::from_millis(250));
+    }
+    r
+}
+pub fn create_headers(json_map: serde_json::Value) -> Result<HashMap<String, String>> {
+    let mut headers: HashMap<String, String> = HashMap::new();
+    for (k, v) in json_map.as_object().ok_or_else(o!())? {
+        headers.insert(k.clone(), v.as_str().ok_or_else(o!())?.to_string());
+    }
+    Ok(headers)
 }
 // returns string in between two strings and returns last index where found
 pub fn _find<'a>(search: &'a str, start: &str, end: &str, i: usize) -> Option<(&'a str, usize)> {
