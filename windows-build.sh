710bd2f0734020fbc081db24a815c5e889ca45ba
baconator696 <169552784+baconator696@users.noreply.github.com>
2025-05-28 03:15:17 +0000

    renamed executable names in release

--- windows-build.sh
+++ windows-build.sh
@@ -2,4 +2,5 @@ apt update &&
     apt install -y gcc-mingw-w64 pkg-config &&
     rustup target add x86_64-pc-windows-gnu &&
     cargo build -r --target x86_64-pc-windows-gnu &&
-    mv /mnt/target/x86_64-pc-windows-gnu/release/cbstream-rust.exe /mnt/cbstream.exe
+    mv /mnt/target/x86_64-pc-windows-gnu/release/cbstream-rust.exe /mnt/cbstream-win-amd64.exe
+.exe
