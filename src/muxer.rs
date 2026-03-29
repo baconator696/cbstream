561fa537cfb17ccc6afb067cbf244cd173982fa1
baconator696 <169552784+baconator696@users.noreply.github.com>
2026-03-29 03:09:55 -0400

    removed support mkvtoolnix

--- src/muxer.rs
+++ src/muxer.rs
@@ -1,4 +1,4 @@
-use crate::{e, h, o, platforms::Platform, s, stream::Stream, util};
+use crate::{e, h, o, platforms::Platform, s, stream::Stream};
 use std::{
     io::{Read, Write},
     path::{Path, PathBuf},
@@ -8,139 +8,12 @@ use std::{
 };
 type Result<T> = result::Result<T, Box<dyn error::Error>>;
 type Hresult<T> = result::Result<T, String>;
-/// file management
-pub struct FileManage(PathBuf);
-impl FileManage {
-    pub fn new(filepath: PathBuf) -> Result<Self> {
-        Ok(Self(filepath))
-    }
-    pub fn filepath(&self) -> &Path {
-        &self.0
-    }
-}
-impl Drop for FileManage {
-    fn drop(&mut self) {
-        match fs::remove_file(&self.0).map_err(e!()) {
-            Err(e) => eprintln!("{}", e),
-            _ => (),
-        };
-    }
-}
-/// checks if mkvtoolnix is installed and returns path of mkvmerger
-fn mkv_exists() -> Result<Option<String>> {
-    let path = if cfg!(target_os = "windows") {
-        let path = mkv_exists_windows().map_err(s!())?;
-        if fs::metadata(&path).is_ok() { Some(path) } else { None }
-    } else {
-        let path = "mkvmerge";
-        match process::Command::new(path).arg("-V").output() {
-            Ok(_) => Some(path.to_string()),
-            Err(_) => None,
-        }
-    };
-    Ok(path)
-}
-#[cfg(not(windows))]
-fn mkv_exists_windows() -> Result<String> {
-    Ok(String::new())
-}
-#[cfg(windows)]
-fn mkv_exists_windows() -> Result<String> {
-    use crate::o;
-    use winreg::RegKey;
-    use winreg::enums::*;
-    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
-    let uninstall_paths = [
-        r"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\MKVToolNix",
-        r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MKVToolNix",
-    ];
-    for path in uninstall_paths {
-        let mkv_key = match hklm.open_subkey_with_flags(path, KEY_READ | KEY_WOW64_64KEY) {
-            Ok(r) => r,
-            _ => continue,
-        };
-        let uninstall_string: String = mkv_key.get_value("UninstallString").map_err(e!())?;
-        let dir_split = uninstall_string.split("\\").collect::<Vec<&str>>();
-        let dir = dir_split.get(..dir_split.len() - 1).ok_or_else(o!())?.join("\\");
-        let path = format!("{}\\mkvmerge.exe", dir);
-        return Ok(path);
-    }
-    Ok("C:\\Program Files\\MKVToolNix\\mkvmerge.exe".to_string())
-}
 fn ffmpeg_exists() -> Result<Option<&'static str>> {
     let path = "ffmpeg";
     match process::Command::new(path).arg("-version").output() {
         Ok(_) => Ok(Some(path)),
         Err(_) => Ok(None),
     }
-}
-/// muxes streams with mkvmerge
-fn mkvmerge(mkvmerge_path: &str, streams: &Vec<Arc<RwLock<Stream>>>, filepath: &Path) -> Result<()> {
-    let mut filepath = filepath.to_path_buf();
-    filepath.set_extension("mkv");
-    // creates arg list for mkvmerge
-    let mut arg_list: Vec<String> = Vec::with_capacity(streams.len() * 2 + 2);
-    arg_list.push("-o".into());
-    arg_list.push(filepath.display().to_string());
-    for stream in streams {
-        let s = &(*stream.read().map_err(s!())?);
-        if s.file.is_some() {
-            arg_list.push(format!("{}", s.stream_path.display()));
-            arg_list.push("+".into());
-        }
-    }
-    arg_list.pop();
-    let json = serde_json::to_string(&arg_list).map_err(e!())?;
-    let mut json_path = util::temp_dir().map_err(s!())?;
-    json_path.push(filepath.file_name().ok_or_else(o!())?);
-    json_path.set_extension("json");
-    fs::write(&json_path, json).map_err(e!())?;
-    let json_file = FileManage::new(json_path).map_err(s!())?;
-    // starts mkvmerge process
-    let mut child = process::Command::new(mkvmerge_path)
-        .arg(format!("@{}", json_file.filepath().display()))
-        .arg("-q")
-        .stderr(process::Stdio::piped())
-        .stdout(process::Stdio::piped())
-        .spawn()
-        .map_err(e!())?;
-    // read from stderr/stdout pipes
-    let mut stdout = child.stdout.take().ok_or_else(o!())?;
-    let mut stderr = child.stderr.take().ok_or_else(o!())?;
-    let stdout_handle = thread::spawn(move || -> Hresult<String> {
-        let mut out = String::new();
-        stdout.read_to_string(&mut out).map_err(e!())?;
-        Ok(out)
-    });
-    let stderr_handle = thread::spawn(move || -> Hresult<String> {
-        let mut out = String::new();
-        stderr.read_to_string(&mut out).map_err(e!())?;
-        Ok(out)
-    });
-    // monitors system memory
-    let mut sys = sysinfo::System::new_all();
-    let exit_status = loop {
-        match child.try_wait().map_err(e!())? {
-            Some(o) => break o,
-            None => (),
-        }
-        sys.refresh_memory();
-        if sys.available_memory() < 200000000 {
-            child.kill().map_err(e!())?;
-            if fs::metadata(&filepath).is_ok() {
-                fs::remove_file(filepath).map_err(e!())?;
-            }
-            return Err("not enough memory, killed mkvmerge").map_err(s!())?;
-        }
-        thread::sleep(time::Duration::from_millis(200));
-    };
-    let stdout = stdout_handle.join().map_err(h!())?.map_err(s!())?;
-    let stderr = stderr_handle.join().map_err(h!())?.map_err(s!())?;
-    // processes output
-    if exit_status.code().ok_or_else(o!())? == 2 {
-        return Err(format!("{}{}", stdout.trim(), stderr.trim())).map_err(s!())?;
-    }
-    return Ok(());
 }
 /// muxes streams with ffmpeg pipe
 fn ffmpeg(ffmpeg_path: &str, streams: &Vec<Arc<RwLock<Stream>>>, filepath: &Path, pf: &Platform) -> Result<()> {
@@ -242,18 +115,12 @@ pub fn muxer(streams: &Vec<Arc<RwLock<Stream>>>, filepath: &Path, filepath_audio
             Ok(_) => return Ok(()),
         }
     }
-    if let Some(mkvmerge_path) = mkv_exists().map_err(s!())? {
-        match mkvmerge(&mkvmerge_path, streams, filepath) {
-            Err(e) => eprintln!("{}", e),
-            Ok(_) => return Ok(()),
-        }
-    }
     local_muxer(streams, filepath, filepath_audio, pf).map_err(s!())?;
     Ok(())
 }
 /// Fallback local muxer
 fn local_muxer(streams: &Vec<Arc<RwLock<Stream>>>, filepath: &Path, filepath_audio: Option<PathBuf>, pf: Platform) -> Result<()> {
-    let extension = match pf {
+    let mut extension = match pf {
         Platform::CB => "ts",
         Platform::MFC => "ts",
         Platform::SC => "mp4",
@@ -261,6 +128,9 @@ fn local_muxer(streams: &Vec<Arc<RwLock<Stream>>>, filepath: &Path, filepath_aud
         Platform::BONGA => "ts",
         Platform::SODA => "mp4",
     };
+    if filepath_audio.is_some() {
+        extension = "mp4";
+    }
     let mut filepath = filepath.to_path_buf();
     filepath.set_extension(extension);
     // creates file
