3fd3fcc1e42626f2f82655b0b2e85330da884e74
baconator696 <169552784+baconator696@users.noreply.github.com>
2026-07-03 03:34:40 -0400

    add ability to choose config json location via arg 1 or env var CONFIG

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,23 @@ mod muxer;
 mod platforms;
 mod stream;
 mod util;
-use std::{path::Path, thread, time::Duration};
+use std::{
+    path::{PathBuf},
+    time::Duration,
+    *,
+};
 
 fn main() {
     const TAG: Option<&str> = option_env!("TAG");
     println!("cbstream {}", TAG.unwrap_or_default());
-    let filename = Path::new("cb-config.json");
+
+    let filename = PathBuf::from(
+        env::args_os()
+            .nth(1)
+            .or_else(|| env::var_os("CONFIG"))
+            .unwrap_or_else(|| "cb-config.json".into()),
+    );
+
     let mut models = config::init(&filename).unwrap();
     while !abort::get().unwrap() {
         models.download().unwrap();
